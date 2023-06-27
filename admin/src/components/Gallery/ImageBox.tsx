cc4153c19e4e583fb8ea8593b2d12ff41316d06c
bNc <kissbence326@gmail.com>
2023-06-27 22:12:55 +0200

    feat(admin:ImageBox.tsx) Adding lazy loading to image

--- admin/src/components/Gallery/ImageBox.tsx
+++ admin/src/components/Gallery/ImageBox.tsx
@@ -3,6 +3,8 @@ import {
   DeleteCheckbox,
   Gallery as GalleryModel,
 } from "../../models/Gallery/Gallery";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 interface Props {
   changeCheckboxHandler: (e: React.ChangeEvent) => void;
@@ -26,16 +28,17 @@ const ImageBox: React.FC<Props> = ({
               data-id={img.id}
               type="checkbox"
               id={img.id}
-              className="absolute z-10 top-[calc(1.25rem/2)] right-[calc(1.25rem/2)] w-5 h-5 rounded-md outline-none bg-white checked:bg-[#636060] active:ring-0 active:ring-transparent focus:ring-0 focus:ring-transparent cursor-pointer transition-all ease-in-out duration-300"
+              className="absolute z-10 top-[calc(1.25rem/2)] right-[calc(1.25rem/2)] w-5 h-5 rounded-3xl outline-none bg-white checked:bg-[#636060] active:ring-0 active:ring-transparent focus:ring-0 focus:ring-transparent cursor-pointer transition-all ease-in-out duration-300"
             />
             <label
               htmlFor={img.id}
               className="w-full h-full cursor-pointer relative"
             >
-              <img
+              <LazyLoadImage
                 src={`store/images/gallery/${img.path}`}
                 alt={img.alt}
-                className="w-full h-full block"
+                className="w-full h-full block rounded-3xl"
+                effect="blur"
               />
 
               <div className="text-dynamicSmall text-center py-2 select-none absolute w-full bottom-0 z-0 bg-white">
