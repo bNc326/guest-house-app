14909897bcfc65e02528a51b34dafe239e2cd4ab
bNc <kissbence326@gmail.com>
2023-06-29 13:12:32 +0200

    fix(EditModal.tsx) fix modal position

--- admin/src/components/modal/EditModal.tsx
+++ admin/src/components/modal/EditModal.tsx
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 import { cloneDeep, isEqual } from "lodash";
 import { ClipLoader, SyncLoader } from "react-spinners";
 import { FaSave } from "react-icons/fa";
+import { GrFormClose } from "react-icons/gr";
 import {
   ALERT_ACTION,
   ALERT_ACTION_TYPE,
@@ -129,7 +130,71 @@ const EditModal: React.FC<{
 
   return (
     <>
-      <Modal show={props.isShow} onClose={closeModalHandler}>
+      <div className="fixed top-0 left-0 flex items-center justify-center w-full h-screen bg-black/50 z-[1500] backdrop-blur-sm">
+        <div className="bg-white rounded-lg w-11/12 p-4 py-8 shadow-xl flex flex-col gap-2">
+          <div className="border-b-2 border-black/20 py-2 flex justify-between items-center">
+            <span className="font-medium">Kizárt dátum szerkesztése</span>
+            <span
+              className="p-1 rounded-md bg-black/10"
+              onClick={() => closeModalHandler()}
+            >
+              <GrFormClose size={24} />
+            </span>
+          </div>
+          <form className="flex flex-col gap-4" onSubmit={formSubmitHandler}>
+            {data && !fetchLoading && (
+              <div className="flex flex-col mobile:flex-row gap-4 items-center">
+                <div className="w-full">
+                  <label htmlFor="startDate">Kezdő dátum</label>
+                  <TextInput
+                    icon={BsFillCalendar2RangeFill}
+                    onChange={onChangeHandler}
+                    name="startDate"
+                    id="startDate"
+                    defaultValue={format(
+                      new Date(data.startDate),
+                      "yyyy-MM-dd"
+                    )}
+                    color="gray"
+                    type="date"
+                  />
+                </div>
+                <div className="w-full">
+                  <label htmlFor="endDate">Vége dátum</label>
+                  <TextInput
+                    icon={BsFillCalendar2RangeFill}
+                    onChange={onChangeHandler}
+                    name="endDate"
+                    id="endDate"
+                    defaultValue={format(new Date(data.endDate), "yyyy-MM-dd")}
+                    color="gray"
+                    type="date"
+                  />
+                </div>
+              </div>
+            )}
+            {fetchLoading && (
+              <div className="w-full flex items-center justify-center py-4">
+                <SyncLoader loading color="#00000080" size={".5rem"} />
+              </div>
+            )}
+            <Button disabled={isObjectEqual} type="submit">
+              <span className="flex gap-1 items-center">
+                {isLoading ? (
+                  <ClipLoader loading={true} color={"white"} size={"1rem"} />
+                ) : (
+                  <FaSave />
+                )}
+                Mentés
+              </span>
+            </Button>
+            <span className="text-black/50 text-md">
+              azonosító: {data?._id}
+            </span>
+          </form>
+        </div>
+      </div>
+      {/* <Modal show={props.isShow} onClose={closeModalHandler}>
         <Modal.Header>
           <span className="font-medium">Kizárt dátum szerkesztése</span>
         </Modal.Header>
@@ -186,7 +251,7 @@ const EditModal: React.FC<{
             </span>
           </form>
         </Modal.Body>
-      </Modal>
+      </Modal> */}
     </>
   );
 };
