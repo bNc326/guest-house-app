7371f9d966a7a198f46c1205a1b76a2ce9fca775
bNc <kissbence326@gmail.com>
2023-06-29 13:12:58 +0200

    fix(Navigation.tsx) fix z-index

--- admin/src/components/Navigation.tsx
+++ admin/src/components/Navigation.tsx
@@ -101,7 +101,7 @@ const Navigation: React.FC<{
         setIsTippOff={setIsTippOff}
       />
 
-      <ul className="gap-4 flex-col hidden tablet:flex sticky top-14 ">
+      <ul className="gap-4 flex-col hidden tablet:flex sticky top-14 z-[1001] ">
         {routePath.map((route, index) => (
           <NavLink
             key={index}
