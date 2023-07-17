757134e08c3a256bc4d06d018c6353a3d2245bdf
Kiss Bence <107045445+bNc326@users.noreply.github.com>
2023-07-17 10:14:11 +0200

    add more debugger lines

--- backend/api/controllers/hotels.js
+++ backend/api/controllers/hotels.js
@@ -29,7 +29,8 @@ export const sendHotels = async (req, res, next) => {
     const body = req.body;
     console.log('body', body)
     console.log('hotelName', body.hotelName)
-    const name = body.hotelName.toLowerCase().replaceAll(" ", "-");
+    const lowerName = body.hotelName.toLowerCase()
+    const name = lowerName..replaceAll(" ", "-");
     console.log('name', name)
     const uniqueId = uuid().split("-")[0];
     body.hotelUUID = `${uniqueId}-${name}`;
