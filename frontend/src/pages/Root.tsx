39a6732133a07685e14335be7c30e377f1344461
bNc <kissbence326@gmail.com>
2023-07-31 14:52:10 +0200

    add <ScrollToTop /> when change route

--- frontend/src/pages/Root.tsx
+++ frontend/src/pages/Root.tsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 import Navigation from "../components/Navigation";
+import ScrollToTop from "ScrollToTop";
 const Root = () => {
   return (
     <>
+      <ScrollToTop />
       <section className="parent relative h-screen">
         <header className="nav-header fixed w-full top-0 z-[1000] bg-palette-3 shadow-md flex flex-col items-center justify-center">
           <Navigation />
